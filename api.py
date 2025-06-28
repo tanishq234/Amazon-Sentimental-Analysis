b54a63fdc68c75e19e1557747ac718e3443b9490
TanishqSaxena234 <133689858+tanishq234@users.noreply.github.com>
2025-06-28 14:58:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -150,4 +150,5 @@ def generate_pie_chart(data):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000)
+    port=int(os.environ.get("PORT",8000)
+    app.run(host='0.0.0.0',port=port)
